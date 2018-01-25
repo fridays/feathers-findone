964af87a7769dfc75e6e96bbb9ddc4dce1f562cc
fridays <fridays@users.noreply.github.com>
2018-01-25 18:20:14 +0100

    es5 compatibility

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-module.exports = () => app => {
-  app.mixins.push(service => {
-    service.findOne = async (params = {}) => {
-      params.query = params.query || {}
-      params.query.$limit = 1
-      const result = await service.find(params)
-      const data = result.data || result
-      return Array.isArray(data) ? data[0] : data
-    }
-  })
+module.exports = function () {
+  return function (app) {
+    app.mixins.push(function (service) {
+      service.findOne = function (params) {
+        params = params || {}
+        params.query = params.query || {}
+        params.query.$limit = 1
+        return service.find(params).then(function (result) {
+          var data = result.data || result
+          return Array.isArray(data) ? data[0] : data
+        })
+      }
+    })
+  }
 }
